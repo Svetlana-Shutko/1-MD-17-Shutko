eac5a03dc6ef36b7928f072de56c795243cf55fb
Svetlana-Shutko <shutkosv@yandex.ru>
2024-02-21 16:18:59 +0300

    2)

--- 2).py
+++ 2).py
@@ -3,9 +3,7 @@ if (nomer % 2 == 0) and (nomer >= 37) and (nomer <= 54):
     print(nomer, "- верхнее боковое место ")
 elif (nomer % 2 != 0) and (nomer >= 37) and (nomer <= 54):
     print(nomer, "- нижнее боковое место ")
-if (nomer % 2 == 0) and (nomer <= 37) and (nomer >= 54):
+if (nomer % 2 == 0) and not((nomer >= 37) and (nomer <= 54)):
     print(nomer, "- верхнее место в купе")
-elif (nomer % 2 != 0) and (nomer <= 37) and (nomer >= 54):
+elif (nomer % 2 != 0) and not((nomer >= 37) and (nomer <= 54)):
     print(nomer, "- нижнее место в купе ")
-elif not ((nomer % 2 != 0) and (nomer <= 37) and (nomer >= 54)):
-    print( "такого места нет")
