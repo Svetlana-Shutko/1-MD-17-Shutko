40f6a135311b1ecb2607548f3a4ec7e4604cc28c
Svetlana-Shutko <shutkosv@yandex.ru>
2024-02-28 16:52:51 +0300

    3zadanie

--- 3).py
+++ 3).py
@@ -1,9 +1,10 @@
-slovo = input("Введите слово:")
 flag = 0
-for bukva in slovo:
-    if bukva == "ф":
-        flag += 1
-if flag > 0:
-    print("Ого! Это редкое слово!")
-else:
-    print("Эх, это не очень редкое слово...")
+while True:
+    slovo = input("Введите слово:")
+    for bukva in slovo:
+        if bukva == "ф":
+            flag += 1
+    if flag > 0:
+        print("Ого! Это редкое слово!")
+    else:
+        print("Эх, это не очень редкое слово...")
