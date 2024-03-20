8cb1a857905c44fd88550f64319e184d278d5106
Svetlana-Shutko <shutkosv@yandex.ru>
2024-03-20 16:24:26 +0300

    20.03.2024

--- 1).py
+++ 1).py
@@ -1,10 +1,10 @@
-#chislo = int(input("Vvedite chislo:"))
-#def proverka(chislo, t=5):
-    #if chislo//t:
-        #print("yes")
-    #else:
-        #print("No")
-#proverka(chislo, t = 7)
+chislo = int(input("Vvedite chislo:"))
+def proverka(chislo, t=5):
+    if chislo//t:
+        print("yes")
+    else:
+        print("No")
+proverka(chislo, t = 7)
 
 def a(chislo1):
     try:
@@ -18,12 +18,12 @@ chislo1 = int(input("Vvedite chislo:"))
 if a(chislo1):
     print("yes")
 
-#def tri():
-    #data = input("vvedite datu")
-    #day, month, year = map(int, data.split("."))
-    #if day * month == int(str(year)[-2:]):
-        #return "yes"
-    #else:
-        #return "no"
-#print(tri())
+def tri():
+    data = input("vvedite datu")
+    day, month, year = map(int, data.split("."))
+    if day * month == int(str(year)[-2:]):
+        return "yes"
+    else:
+        return "no"
+print(tri())
 
